edfb1dc3afd6e95d52d82bdc5882fa4b518292f1
Viacheslav Manulik <viacheslav.manulik@muranosoft.com>
2016-06-29 19:32:50 +0300

    Cleaunup

--- background.js
+++ background.js
@@ -1,21 +1,25 @@
+function copy(text) {
+	var input = document.createElement('textarea');
+	document.body.appendChild(input);
+	
+	input.value = text;
+	input.focus();
+	input.select();
+	document.execCommand('Copy');
+	
+	input.remove();
+}
+
 chrome.browserAction.onClicked.addListener(function() {
 	chrome.tabs.query({
 		currentWindow: true,
 		active: true
 	}, function(tab) {
-	chrome.runtime.onMessage.addListener(function(message) {
-		if (message && message.type == 'copy') {
-			var input = document.createElement('textarea');
-			document.body.appendChild(input);
-			
-			input.value = message.text;
-			input.focus();
-			input.select();
-			document.execCommand('Copy');
-			
-			input.remove();
-		}
-	});
+		chrome.runtime.onMessage.addListener(function(message) {
+			if (message && message.type == 'copy') {
+				copy(message.text);
+			}
+		});
 
 		chrome.tabs.executeScript(null, {file: "content_script.js"});
 	});
