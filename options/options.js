9d86312ee8815002e8add24764b5bd61e515d38e
Viacheslav Manulik <viacheslav.manulik@muranosoft.com>
2016-09-06 20:43:32 +0300

    Options storing implemented

--- options/options.js
+++ options/options.js
@@ -1,32 +1,30 @@
-// Saves options to chrome.storage
 function save_options() {
-  var color = document.getElementById('color').value;
-  var likesColor = document.getElementById('like').checked;
+	
+  var keyboardShortcutEnabled = document.getElementById('keyboard-shortcut-enabled').checked;
+  var keyboardShortcutValue = document.getElementById('keyboard-shortcut-value').value;
+  
   chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
+    keyboardShortcutEnabled: keyboardShortcutEnabled,
+    keyboardShortcutValue: keyboardShortcutValue
+  }, function() { });
+}
+
+function restore_options() {
+	
+  chrome.storage.sync.get({
+    keyboardShortcutEnabled: true,
+    keyboardShortcutValue: 'ctrl-shift-c'
+  }, function(items) {  
+    document.getElementById('keyboard-shortcut-enabled').checked = items.keyboardShortcutEnabled;
+    document.getElementById('keyboard-shortcut-value').value = items.keyboardShortcutValue;
   });
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true
-  }, function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
-  });
-}
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+document.addEventListener('DOMContentLoaded', function(){
+	
+	document.getElementById('keyboard-shortcut-enabled').addEventListener('change', save_options);
+	document.getElementById('keyboard-shortcut-value').addEventListener('change', save_options);
+	
+	restore_options();
+});
+
